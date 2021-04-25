8c2ca3d17bae69213d5e478d5a8002857561b98a
JuanJakobo <github@johannsn.com>
2021-04-25 15:51:02 +0200

    handle keyevents

--- src/handler/eventHandler.cpp
+++ src/handler/eventHandler.cpp
@@ -39,8 +39,8 @@ int EventHandler::eventDistributor(const int type, const int par1, const int par
 {
     if (ISPOINTEREVENT(type))
         return EventHandler::pointerHandler(type, par1, par2);
-    //else if (ISKEYEVENT(type))
-        //return EventHandler::keyHandler(type, par1, par2);
+    else if (ISKEYEVENT(type))
+        return EventHandler::keyHandler(type, par1, par2);
 
     return 0;
 }
@@ -95,5 +95,42 @@ int EventHandler::pointerHandler(const int type, const int par1, const int par2)
             return 1;
         }
     }
+    return 0;
+}
+
+int EventHandler::keyHandler(const int type, const int par1, const int par2)
+{
+    //menu button
+    if (par1 == 23)
+    {
+        _listView->draw();
+        _entryView = nullptr;
+        PartialUpdate(_menu.getContentRect()->x, _menu.getContentRect()->y, _menu.getContentRect()->w, _menu.getContentRect()->h);
+        return 1;
+    }
+    if (_entryView != nullptr)
+    {
+        //left button -> pre page
+        if (par1 == 24)
+        {
+            //next page with text, how to handle images?
+        }
+        //right button -> next page
+        else if (par1 == 25)
+        {
+        }
+    }
+    else if (_listView != nullptr)
+    {
+        //left button -> pre page
+        if (par1 == 24)
+        {
+        }
+        //right button -> next page
+        else if (par1 == 25)
+        {
+        }
+    }
+
     return 0;
 }
