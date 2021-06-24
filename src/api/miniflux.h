5ebf6aceb2d62819a0c926e94c6dd5ac18197237
JuanJakobo <github@johannsn.com>
2021-06-24 12:32:42 +0200

    update miniflux api to include futher endpoints

--- src/api/miniflux.h
+++ src/api/miniflux.h
@@ -22,9 +22,26 @@
 class Miniflux
 {
 public:
+    /**
+    * creatas a new Miniflux object containing the url and token to access the api
+    *
+    * @param url 
+    * @param token 
+    *  
+    */
     Miniflux(const std::string &url, const std::string &token);
 
     std::vector<entry> getEntries(const std::string &filter);
+
+    feedIcon getFeedIcon(int feedID);
+
+    /**
+    * Refreshes all feeds in the background
+    *
+    *  
+    * @return true if feeds upgrade was initiated
+    */
+    bool refreshAllFeeds();
 
     bool markUserEntriesAsRead(int userID);
 
@@ -33,6 +50,8 @@ public:
 private:
     std::string _url;
     std::string _token;
+
+    bool put(const std::string &apiEndpoint, const std::string &data);
 
     nlohmann::json get(const std::string &apiEndpoint);
 };
