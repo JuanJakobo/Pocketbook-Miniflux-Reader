a4870c3b109462bfd9df51f7923e54212744b1ba
JuanJakobo <34421964+JuanJakobo@users.noreply.github.com>
2021-06-24 12:34:41 +0200

    show reading time only if is bigger 0

--- src/ui/minifluxViewEntry.cpp
+++ src/ui/minifluxViewEntry.cpp
@@ -17,17 +17,16 @@ MinifluxViewEntry::MinifluxViewEntry(int page, const irect &rect, const entry &i
 void MinifluxViewEntry::draw(const ifont *entryFont, const ifont *entryFontBold, int fontHeight)
 {
     SetFont(entryFontBold, BLACK);
-    //TODO catch if to big
+    //TODO feed icon infront of text
     int heightOfTitle = TextRectHeight(_position.w, _entry->title.c_str(), 0);
     DrawTextRect(_position.x, _position.y, _position.w, heightOfTitle, _entry->title.c_str(), ALIGN_LEFT);
 
     SetFont(entryFont, BLACK);
-    //TODO feed icon infront of text
-    //DrawTextRect(_position.x, _position.y + fontHeight, _position.w, fontHeight, _entry->url.c_str(), ALIGN_LEFT);
     if (_entry->starred)
         DrawTextRect(_position.x, _position.y + heightOfTitle, _position.w, fontHeight, "starred", ALIGN_LEFT);
     DrawTextRect(_position.x, _position.y + heightOfTitle, _position.w, fontHeight, _entry->status.c_str(), ALIGN_RIGHT);
-    DrawTextRect(_position.x, _position.y + heightOfTitle + fontHeight, _position.w, fontHeight, (std::to_string(_entry->reading_time) + " min").c_str(), ALIGN_RIGHT);
+    if(_entry->reading_time > 0)
+        DrawTextRect(_position.x, _position.y + heightOfTitle + fontHeight, _position.w, fontHeight, (std::to_string(_entry->reading_time) + " min").c_str(), ALIGN_RIGHT);
 
     int line = (_position.y + _position.h) - 1;
     DrawLine(0, line, ScreenWidth(), line, BLACK);
